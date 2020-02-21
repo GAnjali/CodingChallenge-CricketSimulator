4d745b7d46e2e02ef1b49798fcbdc4cce98e9a22
ganjali <GAnjali@github.com>
2020-02-21 15:33:24 +0530

    Implement simulate match functionality

--- src/main/java/models/Match.java
+++ src/main/java/models/Match.java
@@ -1,24 +1,22 @@
 package models;
 
+import exceptions.PlayerNotFoundException;
+import gamestrategy.RandomWeightedGameStrategy;
+import rules.Rule;
+
+import java.util.List;
+
 public class Match {
     private final String playingTeam;
     private final String opposingTeam;
     private final int overs;
-    private int wicketsLeft;
+    private int wickets;
     private int runNeededToWin;
-    private Player stricker;
-    private Player nonStricker;
-    private int runsCount;
-    private int ballsPlayed;
 
-    public Match(String playingTeam, String opposingTeam, Player stricker, Player nonStricker, int runsCount, int ballsPlayed, int wicketsLeft, int runNeededToWin, int overs) {
+    public Match(String playingTeam, String opposingTeam, int wickets, int runNeededToWin, int overs) {
         this.playingTeam = playingTeam;
         this.opposingTeam = opposingTeam;
-        this.stricker = stricker;
-        this.nonStricker = nonStricker;
-        this.runsCount = runsCount;
-        this.ballsPlayed = ballsPlayed;
-        this.wicketsLeft = wicketsLeft;
+        this.wickets = wickets;
         this.runNeededToWin = runNeededToWin;
         this.overs = overs;
     }
@@ -31,8 +29,8 @@ public class Match {
         return opposingTeam;
     }
 
-    public int getWicketsLeft() {
-        return wicketsLeft;
+    public int getWickets() {
+        return wickets;
     }
 
     public int getRunNeededToWin() {
@@ -41,5 +39,64 @@ public class Match {
 
     public int getOvers() {
         return overs;
+    }
+
+    public void simulate(List<Player> players, RandomWeightedGameStrategy gameStrategy, Rule[] rules) throws PlayerNotFoundException {
+        int totalScore = 0;
+        MatchStatus status = getInitialStatusOfMatch(players);
+        while (!isMatchCompleted(status)) {
+            Player striker = status.getCurrentStriker();
+            int scoredRuns = gameStrategy.getScoredRuns(striker);
+            updateStatus(scoredRuns, striker, status);
+            status = processNextMove(status, players, rules);
+            if (status.getCurrentWicketLeft() == 0 || totalScore > status.getCurrentRunsToWin())
+                break;
+        }
+    }
+
+    private MatchStatus getInitialStatusOfMatch(List<Player> players) {
+        return new MatchStatus(players.get(0), players.get(1), 0, 4, 0, 40, false, 0);
+    }
+
+    private boolean isMatchCompleted(MatchStatus status) {
+        return status.getCurrentBallsPlayed() >= getTotalBalls();
+    }
+
+    private void updateStatus(int runsScored, Player striker, MatchStatus status) {
+        if (runsScored == 7) {
+            updateStatusWhenStrickerGetsOut(striker, status);
+        } else {
+            updateStatusWhenStrikerScoredRuns(runsScored, striker, status);
+        }
+        increaseBallCount(striker, status);
+    }
+
+    private void updateStatusWhenStrickerGetsOut(Player striker, MatchStatus status) {
+        striker.setOut(true);
+        status.setCurrentPlayerIsOut(true);
+        status.setCurrentWicketLeft(status.getCurrentWicketLeft() - 1);
+    }
+
+
+    private void updateStatusWhenStrikerScoredRuns(int runsScored, Player striker, MatchStatus status) {
+        striker.setTotalRuns(striker.getTotalRuns() + runsScored);
+        status.setCurrentRunCount(status.getCurrentRunCount() + runsScored);
+        status.setCurrentRunsToWin(status.getCurrentRunsToWin() - runsScored);
+    }
+
+    private void increaseBallCount(Player striker, MatchStatus status) {
+        striker.setTotalBallsPlayed(striker.getTotalBallsPlayed() + 1);
+        status.setCurrentBallsPlayed(status.getCurrentBallsPlayed() + 1);
+    }
+
+    private MatchStatus processNextMove(MatchStatus status, List<Player> players, Rule[] rules) throws PlayerNotFoundException {
+        for (Rule rule : rules) {
+            status = rule.processStatus(status, players);
+        }
+        return status;
+    }
+
+    private int getTotalBalls() {
+        return this.overs * 6;
     }
 }
--- src/test/java/models/MatchTest.java
+++ src/test/java/models/MatchTest.java
@@ -1,4 +1,47 @@
 package models;
 
+import exceptions.PlayerNotFoundException;
+import gamestrategy.RandomWeightedGameStrategy;
+import org.junit.Before;
+import org.junit.Test;
+import rules.ChangeStrikeRule;
+import rules.PlayerOutRule;
+import rules.Rule;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
 public class MatchTest {
+
+    List<Player> players;
+    MatchStatus status;
+    Match match;
+    RandomWeightedGameStrategy runStrategy;
+    Rule[] rules;
+
+    @Before
+    public void init() {
+        players = new ArrayList<>();
+        match = new Match("Bengaluru", "Chennai", 4, 40, 4);
+        runStrategy = new RandomWeightedGameStrategy();
+        rules = new Rule[2];
+        rules[0] = new PlayerOutRule();
+        rules[1] = new ChangeStrikeRule();
+    }
+
+    @Test
+    public void testWithHighProbablityOfEachPlayerGettingOut() throws PlayerNotFoundException {
+        players.add(new Player("Kirat Boli", Arrays.asList(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 93.0), 0, 0, false));
+        players.add(new Player("NS Nodhi", Arrays.asList(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 93.0), 0, 0, false));
+        players.add(new Player("R Rumrah", Arrays.asList(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 93.0), 0, 0, false));
+        players.add(new Player("Shashi Henra", Arrays.asList(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 93.0), 0, 0, false));
+        status = new MatchStatus(players.get(0), players.get(1), 0, 4, 0, 40, false, 0);
+        match.simulate(players, runStrategy, rules);
+        for (Player player : players) {
+            assertTrue(player.isOut());
+        }
+    }
 }
