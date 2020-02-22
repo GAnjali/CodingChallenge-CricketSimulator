bccdd50bc0e2a0c4f267b1976051b7cb5cc7c85f
ganjali <GAnjali@github.com>
2020-02-22 10:10:34 +0530

    Intialize main method with required specifications then simulate the match

--- src/main/java/CricketSimulationMain.java
+++ src/main/java/CricketSimulationMain.java
@@ -1,15 +1,27 @@
+import commentary.Commentary;
+import exceptions.PlayerNotFoundException;
+import gamestrategy.RandomWeightedGameStrategy;
 import models.Game;
 import models.Match;
 import models.Player;
+import rules.ChangeStrikeRule;
+import rules.PlayerOutRule;
+import rules.Rule;
 
 import java.util.List;
 
 public class CricketSimulationMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws PlayerNotFoundException {
         Game game = new Game();
         List<Player> players = game.intializePlayers();
-        Match match = game.intializeMatch(players);
+        RandomWeightedGameStrategy runStrategy = new RandomWeightedGameStrategy();
+        Rule[] rules = new Rule[2];
+        rules[0] = new PlayerOutRule();
+        rules[1] = new ChangeStrikeRule();
+        Match match = game.intializeMatch();
+        Commentary commentary = new Commentary();
+        match.simulate(players, runStrategy, rules, commentary);
     }
 }
 
