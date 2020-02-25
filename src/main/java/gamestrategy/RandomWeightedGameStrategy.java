5b3f1687e618fc8ffe794dc41646eeac07a71630
ganjali <GAnjali@github.com>
2020-02-25 15:27:10 +0530

    Refactor random number generator function

--- src/main/java/gamestrategy/RandomWeightedGameStrategy.java
+++ src/main/java/gamestrategy/RandomWeightedGameStrategy.java
@@ -5,18 +5,17 @@ import models.Player;
 import java.util.List;
 import java.util.Random;
 
+import static helper.CrickerSimulatorConstants.RANGE;
+
 public class RandomWeightedGameStrategy implements GameStrategy {
+    private Random random = new Random();
+
     public int getScoredRuns(Player strikePlayer) {
-        return generateRandomRuns(strikePlayer.getProbability(), generateRandomNumber(strikePlayer));
+        return generateRandomRuns(strikePlayer.getProbability(), generateRandomNumber());
     }
 
-    private double generateRandomNumber(Player strikePlayer) {
-        Random random = new Random();
-        return random.nextInt((int) getSum(strikePlayer.getProbability()) + 1);
-    }
-
-    private double getSum(List<Double> probability) {
-        return probability.stream().mapToDouble(Double::doubleValue).sum();
+    private double generateRandomNumber() {
+        return random.nextInt(RANGE);
     }
 
     private int generateRandomRuns(List<Double> probability, double randomNumber) {
--- src/main/java/helper/CrickerSimulatorConstants.java
+++ src/main/java/helper/CrickerSimulatorConstants.java
@@ -3,4 +3,5 @@ package helper;
 public class CrickerSimulatorConstants {
     public static final int NO_OF_BALLS_PER_OVER = 6;
     public static final int INVALID_RUN = 7;
+    public static final int RANGE = 101;
 }
