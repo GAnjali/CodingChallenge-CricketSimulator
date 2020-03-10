e03e8a9f7b57bc25c2c307ac78e514ac8920ece6
ganjali <GAnjali@github.com>
2020-03-10 07:10:57 +0530

    Fix IntializerTest issues according to the Intializer

--- src/test/java/helper/InitializerTest.java
+++ src/test/java/helper/InitializerTest.java
@@ -1,45 +1,25 @@
 package helper;
 
-import config.Config;
-import models.Player;
+import models.Match;
 import org.junit.Before;
 import org.junit.Test;
-import rules.Rule;
 
 import java.io.IOException;
-import java.util.List;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class InitializerTest {
     Initializer initializer;
-    List<Player> players;
-    Rule[] rules;
-    Config config;
+    Match match;
 
     @Before
     public void init() throws IOException {
         initializer = new Initializer();
-        config = new Config();
-        config.loadProperties();
-    }
-
-    private int getNoOfPlayers() {
-        return Integer.parseInt(config.getValue("NO_OF_PLAYERS"));
     }
 
     @Test
-    public void testShouldVerifyPlayersWhenInitializePlayersCalled() {
-        players = initializer.createPlayers();
-        for (int playerIndex = 0; playerIndex < getNoOfPlayers(); playerIndex++)
-            assertEquals(config.getValue("PLAYER_" + (playerIndex + 1)), players.get(playerIndex).getName());
-    }
-
-    @Test
-    public void testShouldVerifyRulesWhenCreateRulesCalled() {
-        rules = initializer.createRules();
-        assertNotNull(rules[0]);
-        assertNotNull(rules[1]);
+    public void testShouldVerifyMatchWhenInitializeMatchCalled() {
+        match = initializer.initializeMatch();
+        assertNotNull(match);
     }
 }
